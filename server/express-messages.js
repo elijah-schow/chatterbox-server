98ab709eb82c782baea0b594da45e4fa04ff216c
Elijah Schow <elijah.schow@gmail.com>
2017-02-21 17:57:40 -0600

    Save and load chat messages from a file

--- server/express-messages.js
+++ server/express-messages.js
@@ -1,7 +1,17 @@
+// Modules
 var randomstring = require('randomstring');
+var fs = require('fs');
 
+// Public API
+exports.get = get;
+exports.post = post;
+
+// Setup
 _data = [];
+_path = './data/messages.json';
+_load();
 
+// Public Methods
 function get() {
   return _data;
 }
@@ -16,11 +26,36 @@ function post(data) {
     'roomname': decodeURI(data.roomname) || 'lobby',
     'createdAt': createdAt
   });
+  _save();
   return {
     'objectId': objectId,
     'createdAt': createdAt
   };
 }
 
-exports.get = get;
-exports.post = post;
+// Private methods
+function _save() {
+  data = JSON.stringify(_data);
+  fs.writeFile(_path, data, function(err){
+    if(err) {
+      console.log('DATABASE FAILED TO SAVE\n', error);
+    }
+  });
+}
+
+function _load() {
+  fs.readFile(_path, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        console.log('No database file found.');
+        return;
+      } else {throw err;}
+    }
+    try {
+      _data = JSON.parse(data);
+    } catch(error){
+      console.log('DATABASE FAILED TO LOAD\n', error);
+    }
+
+  });
+}
